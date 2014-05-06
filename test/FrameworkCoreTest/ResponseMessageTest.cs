2d5b8f4f32b5e72a84c83608a4068f25dea23918
JamesYing <james@taogame.com>
2014-05-06 17:31:22 +0800

    responsetest

---
+++ test/FrameworkCoreTest/ResponseMessageTest.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WX.Model;
+using Xunit;
+
+namespace FrameworkCoreTest
+{
+    public class ResponseMessageTest
+    {
+        [Fact]
+        public void ResponseImageMessageTest()
+        {
+            var response = new ResponseImageMessage
+            {
+                FromUserName = "james",
+                ToUserName = "ying",
+                CreateTime = 123123,
+                Image = new ResponageImage
+                {
+                    MediaId = "123"
+                }
+            };
+
+            Console.WriteLine(response.Serializable());
+        }
+
+        [Fact]
+        public void ResponseVoiceMessageTest()
+        {
+            var response = new ResponseVoiceMessage
+            {
+                FromUserName = "jamesying",
+                ToUserName = "ying",
+                CreateTime = 123123,
+                Voice = new ResponseVoice
+                {
+                    MediaId = "123123"
+                }
+            };
+
+            Console.WriteLine(response.Serializable());
+        }
+
+        [Fact]
+        public void ResponseVideoMessageTest()
+        {
+            var response = new ResponseVideoMessage
+            {
+                FromUserName = "jamesying",
+                ToUserName = "ying",
+                CreateTime = 123123,
+                Video = new ResponseVideo
+                {
+                    MediaId = "123123",
+                    Description = "123123",
+                    Title = "123123"
+                }
+            };
+
+            Console.WriteLine(response.Serializable());
+        }
+
+        [Fact]
+        public void ResponseMusicMessageTest()
+        {
+            var response = new ResponseMusicMessage
+            {
+                FromUserName = "jamesying",
+                ToUserName = "ying",
+                CreateTime = 123123,
+                Music = new ResponseMusic
+                {
+                    MusicURL = "asdfasdf",
+                    HQMusicUrl = "asdfasdf",
+                    ThumbMediaId = "asdfasdf",
+                    Description = "123123",
+                    Title = "123123"
+                }
+            };
+
+            Console.WriteLine(response.Serializable());
+        }
+    }
+}
