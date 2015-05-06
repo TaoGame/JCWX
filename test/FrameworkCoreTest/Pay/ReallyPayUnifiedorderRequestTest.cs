2ba503c579576fff7aeafdcb8d9abe85142477cb
JamesYing <james@taogame.com>
2015-05-06 15:09:57 +0800

    测试【统一下单】接口，可提交至正式环境

---
+++ test/FrameworkCoreTest/Pay/ReallyPayUnifiedorderRequestTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WX.Pay.Request;
+using WX.Pay.Response;
+
+namespace FrameworkCoreTest.Pay
+{
+    public class ReallyPayUnifiedorderRequestTest : PayTest<PayUnifiedOrderRequest, PayUnifiedOrderResponse>
+    {
+        protected override PayUnifiedOrderRequest GetRequest()
+        {
+            var date = DateTime.Today;
+
+            return new PayUnifiedOrderRequest
+            {
+                AppId = "xxxx",
+                MchId = "xxxx",
+                PayApiSecret = "xxxx",
+                Attach = "测试支付订单",
+                Body = "测试支付订单",
+                Detail = "测试支付订单",
+                DeviceInfo = "test pay order",
+                NonceStr = "123123",
+                NotifyUrl = "http://www.sh-bus.com",
+                OutTradeNo = "DB123123123",
+                ProductId = "123123",
+                SpbillCreateIp = "127.0.0.1",
+                TimeStart = date.ToString("yyyyMMddHHmmss"),
+                TimeExpire = date.AddDays(4).ToString("yyyyMMddHHmmss"),
+                TotalFee = 100,
+                TradeType = "NATIVE"
+            };
+        }
+
+        protected override string GetResponseXml()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
