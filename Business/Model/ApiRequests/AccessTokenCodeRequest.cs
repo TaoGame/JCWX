69029e7045a0b05266fb550ccf27c57b60933005
应晔 <yingye001@huazhu.com>
2018-06-11 16:39:57 +0800

    添加网页授权接口

---
+++ Business/Model/ApiRequests/AccessTokenCodeRequest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WX.Model.ApiResponses;
+
+namespace WX.Model.ApiRequests
+{
+    public class AccessTokenCodeRequest : ApiRequest<AccessTokenCodeResponse>
+    {
+        public string Code { get; set; }
+        public string AppId { get; set; }
+        public string AppSecret { get; set; }
+
+        protected override string UrlFormat => "https://api.weixin.qq.com/sns/oauth2/access_token?appid={0}&secret={1}&code={2}&grant_type=authorization_code";
+
+        protected override bool NeedToken => false;
+
+        internal override string Method => GETMETHOD;
+
+        public override string GetPostContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override string GetUrl()
+        {
+            return String.Format(UrlFormat, AppId, AppSecret, Code);
+        }
+    }
+}
---
+++ Business/Model/ApiResponses/AccessTokenCodeResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WX.Model.ApiResponses
+{
+    public class AccessTokenCodeResponse : ApiResponse
+    {
+        public string Access_Token { get; set; }
+        public int Expires_in { get; set; }
+        public string Refresh_token { get; set; }
+        public string OpenId { get; set; }
+        public string Scope { get; set; }
+    }
+}
